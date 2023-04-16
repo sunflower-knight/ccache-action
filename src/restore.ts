e4471de7eb63bc3860252643e1005aa81c359977
sunflower-knight <dglz.hyj@qq.com>
2023-04-16 14:28:19 +0800

    change window sccache path

--- src/restore.ts
+++ src/restore.ts
@@ -51,7 +51,7 @@ async function configure(ccacheVariant : string) : Promise<void> {
     core.info("Cccache config:");
     await execBash("ccache -p");
   } else {
-    const options = `SCCACHE_IDLE_TIMEOUT=0 SCCACHE_DIR='${ghWorkSpace}'/.sccache SCCACHE_CACHE_SIZE='${maxSize}'`;
+    const options = `SCCACHE_IDLE_TIMEOUT=0 SCCACHE_DIR='${ghWorkSpace}'\\.sccache SCCACHE_CACHE_SIZE='${maxSize}'`;
     await execBash(`env ${options} sccache --start-server`);
   }
 
